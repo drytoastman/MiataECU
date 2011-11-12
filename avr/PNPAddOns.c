0a5cb2879bb1f856a6d4812aeed468963848829d
Brett Wilson <drytoastman@gmail.com>
2011-11-12 15:41:40 -0800

    establish base AVR project

---
+++ avr/PNPAddOns.c
@@ -0,0 +1,16 @@
+/*
+ * PNPAddOns.c
+ *
+ * Created: 11/12/2011 3:32:50 PM
+ *  Author: bwilson
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
